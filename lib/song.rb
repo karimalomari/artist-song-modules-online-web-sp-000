a40332146c290eac53210040c7a1c7ea95a2568b
Karim Alomari <karim.alomari@gmail.com>
2020-09-18 19:17:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 class Song
+  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
@@ -17,5 +19,4 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
 end
